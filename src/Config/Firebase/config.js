f25d2e32a370afd59260cd000a5ca4c9670f4d90
Eddie Raskin <eddie.raskin@gmail.com>
2020-07-01 08:30:07 -0700

    almost finished, note added w/o text

---
+++ src/Config/Firebase/config.js
@@ -0,0 +1,9 @@
+export const DB_CONFIG = {
+  apiKey: "AIzaSyBdCkRmkO4SWKHFIrUSJgTXUcKa93iGGEI",
+  authDomain: "react-notes-fada5.firebaseapp.com",
+  databaseURL: "https://react-notes-fada5.firebaseio.com",
+  projectId: "react-notes-fada5",
+  storageBucket: "react-notes-fada5.appspot.com",
+  messagingSenderId: "414733525387",
+  appId: "1:414733525387:web:73d367e73f8014c56c4f8e"
+}
